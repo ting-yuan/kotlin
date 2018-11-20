46d0534fbaed6140743cf5451193850a3f6515ed
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-11-20 15:35:43 +0100

    ~enum

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/groovyInterop/src/main/kotlin/MyKotlinClass.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/groovyInterop/src/main/kotlin/MyKotlinClass.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     System.out.println(ClassWithReferenceToInner().f1(null))
     System.out.println(ClassWithReferenceToInner().f2(null))
 
-    //Tests for ASM 7 migration
+    //ASM 7 migration test
     System.out.println(Outer().Nested("123"))
     System.out.println(GEnum.FOO)
 }
