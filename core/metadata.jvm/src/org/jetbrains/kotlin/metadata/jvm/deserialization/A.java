32c93c9884d843ada6043ba30592a95ea25ccf43
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-11-21 15:56:27 +0300

    -- test

---
+++ core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/A.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.metadata.jvm.deserialization;
+
+// PSI_ELEMENT: com.intellij.psi.PsiField
+// OPTIONS: writeAccess
+public abstract class A {
+    public static int foo = 12;
+}
---
+++ core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/javaStaticFieldUsages.1.kt
@@ -0,0 +1,6 @@
+package org.jetbrains.kotlin.metadata.jvm.deserialization
+
+fun test() {
+    A.foo = 12
+    println(A.foo)
+}
